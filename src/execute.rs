2bf63254790bc151054728018a5447a69b82a27e
Florian Roks <froks@users.noreply.github.com>
2022-11-14 20:39:46 +0100

    started work on integrating scripting engine

--- src/execute.rs
+++ src/execute.rs
@@ -1,13 +1,10 @@
 use crate::http_file_parser::parse_http_file;
 use anyhow::{anyhow, Context, Result};
 use std::path::PathBuf;
+use crate::http_request_executor::ExecutionContext;
 
 pub fn execute_http_files(files: Vec<&PathBuf>) -> Result<()> {
-    let client = reqwest::blocking::Client::builder()
-        .http1_title_case_headers()
-        .build()
-        .map_err(|e| anyhow!("{:?}", e))?;
-    let context = crate::http_request_executor::ExecutionContext { client: &client };
+    let context = ExecutionContext::new()?;
     for p in files {
         if !p.is_file() {
             // let filename = p.file_name().unwrap().to_str().unwrap();
